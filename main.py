059544529f1bfc78f1992bfd4bcd93a235d8d4f6
Inshalmunaf <inshalmunaf@gmail.com>
2025-10-20 19:59:14 +0500

    Modular adjustment added for model_training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.Stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.Stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.Stage_03_model_training import ModelTrainingPipeine
 
 
 STAGE_NAME = 'Data Ingestion Stage'
@@ -21,6 +22,16 @@ try:
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<< \n\n==========')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    obj = ModelTrainingPipeine()
+    obj.main()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<< \n\n==========')
+
 except Exception as e:
     logger.exception(e)
     raise e
